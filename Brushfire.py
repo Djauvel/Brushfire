9c9f06610b5853151a82ca22524f2f7155ff5eee
Peter Plass Jensen <Petaarz@hotmail.com>
2024-03-08 20:50:19 +0100

    Brushfire file creation

---
+++ Brushfire.py
@@ -0,0 +1,19 @@
+import matplotlib as plt
+import numpy as np
+
+# Initialize an empty list to store the configuration space
+cspace = []
+
+
+# Parse cspace txt and save in memory
+with open("cspace_low_res.txt", "r") as file:
+    # Read each line in the file
+    for line in file:
+        # Split the line into individual entries
+        entries = line.split()
+
+        # Convert entries into floats
+        entries = [float(entry) for entry in entries]
+
+        cspace.append(entries)
+
