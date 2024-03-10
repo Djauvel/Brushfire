7a7611341c69dd89e1f1d5b26572428f69931f8c
Peter Plass Jensen <Petaarz@hotmail.com>
2024-03-10 18:54:20 +0100

    Separation of obstacle layers

--- Brushfire.py
+++ Brushfire.py
@@ -12,10 +12,27 @@ with open("cspace_low_res.txt", "r") as file:
         entries = line.split()
 
         # Convert entries into floats
-        entries = [float(entry) for entry in entries]
+        entries = [int(float(entry)) for entry in entries]
 
         cspace.append(entries)
 
-cspace_array = np.array(cspace)
+# Convert cspace nested lists to a numpy array
+# Remove all float notation
+cspace_array = np.round(np.array(cspace)).astype(int)
 
-print(cspace_array.max())
+# Instatiate an array to store each obstacle layer
+obstacles = np.ndarray((cspace_array.shape[0],cspace_array.shape[1],np.max(cspace_array)+1))
+
+#Separate each layer
+for i in range(cspace_array.shape[0]-1):
+    for j in range(cspace_array.shape[1]-1):
+        #print(f"Dimensions: {cspace_array.shape}")
+        #print(f"i: {i}, j: {j}")
+        #print(f"cspace_array: {cspace_array[i,j]}")
+        obstacles[i,j,cspace_array[i,j]] = cspace_array[i,j]
+
+
+
+#Test print
+filepath = "testOutput.txt"
+np.savetxt(filepath, obstacles[:,:,2], fmt = "%d")
