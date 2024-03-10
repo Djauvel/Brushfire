6c3a947c57795916586f89a603ce2ee56604e30f
Peter Plass Jensen <Petaarz@hotmail.com>
2024-03-10 20:12:47 +0100

    Voronoi function

--- Brushfire.py
+++ Brushfire.py
@@ -15,28 +15,29 @@ with open("cspace_low_res.txt", "r") as file:
         entries = [int(float(entry)) for entry in entries]
 
         cspace.append(entries)
-
 # Convert cspace nested lists to a numpy array
 # Remove all float notation
 cspace_array = np.round(np.array(cspace)).astype(int)
 
-# Dimensions identical to the source cpsace array, but with 3 dimensions
-dimensions = (cspace_array.shape[0], cspace_array.shape[1], np.max(cspace_array)+1)
+def voronoi(obstacles):
+    # This function should contain the Brushfire algorithm implementation
+    # The function will take an array of values that represent the configuration 
+    # space and associated obstacles
+    # Dimensions identical to the source cpsace array, but with 3 dimensions
+    dimensions = (obstacles.shape[0], obstacles.shape[1], np.max(obstacles)+1)
 
-# Instatiate an array to store each obstacle layer
-obstacles = np.zeros(dimensions)
+    # Instatiate an array to store each obstacle layer
+    obstacle_layers = np.zeros(dimensions)
 
+    #Separate each layer
+    for i in range(obstacles.shape[0]):
+        for j in range(obstacles.shape[1]):
+            if obstacles[i,j] != 0:
+                obstacle_layers[i,j,obstacles[i,j]] = 1
 
-#Separate each layer
-for i in range(cspace_array.shape[0]):
-    for j in range(cspace_array.shape[1]):
-        #print(f"Dimensions: {cspace_array.shape}")
-        #print(f"i: {i}, j: {j}")
-        #print(f"cspace_array: {cspace_array[i,j]}")
-        obstacles[i,j,cspace_array[i,j]] = cspace_array[i,j]
 
 
 
 #Test print
 filepath = "testOutput.txt"
-np.savetxt(filepath, obstacles[:,:,3], fmt = "%d")
+np.savetxt(filepath, obstacles[:,:,1], fmt = "%d")
